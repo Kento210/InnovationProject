3d7da0713b48196222aa12b1ccd4d5ea206d23e0
Kento <T121015@ed.sus.ac.jp>
2023-07-05 02:52:04 +0900

    add

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import ShopList from './components/ShopList';
-import getShops from './getShops';
+import { useForm } from 'react-hook-form';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 
-class App extends React.Component {
+Amplify.configure(awsconfig);
+
+
+export default function AuthForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email", { required: "Email is required", pattern: { value: /^\S+@\S+$/i, message: "Invalid email address" } })} />
+      {errors.email && <p>{errors.email.message}</p>}
+      
+      <select {...register("grade", { required: "Grade is required" })}>
+        <option value="">Select...</option>
+        <option value="1">1st Grade</option>
+        <option value="2">2nd Grade</option>
+        <option value="3">3rd Grade</option>
+        // 他のオプションを追加...
+      </select>
+      {errors.grade && <p>{errors.grade.message}</p>}
+
+      <input type="submit" />
+    </form>
+  );
 }
--- app/src/getShops.js
+++ app/src/getShops.js
@@ -1,24 +1,15 @@
-import AWS from 'aws-sdk';
+import { DynamoDB } from 'aws-sdk';
 
-const getShops = async () => {
-  AWS.config.update({
-    region: 'us-west-2',  // 使用するAWSリージョンを設定します
-    // 必要に応じて、認証情報を設定します。
-    // accessKeyId: 'your-access-key',
-    // secretAccessKey: 'your-secret-access-key',
-  });
+const dynamoDB = new DynamoDB.DocumentClient();
 
-  const docClient = new AWS.DynamoDB.DocumentClient();
+async function getShopData(ShopID) {
+    const params = {
+        TableName: 'ip-Shoplist',  // あなたのDynamoDBテーブル名
+        Key: { storeid }
+    };
 
-  const params = {
-    TableName: 'Shops',  // DynamoDBのテーブル名を設定します
-  };
+    const response = await dynamoDB.get(params).promise();
 
-  try {
-    const data = await docClient.scan(params).promise();
-    return data.Items;
-  } catch (err) {
-    console.log('Error', err);
-  }
-};
+    return response.Item;
+}
 
